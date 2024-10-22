4111b1b2d9621224681b870a9b548705644b2263
RobSis <siska.robert@gmail.com>
2024-10-22 10:32:45 +0200

    Update AsyncComponentLoader.ts

--- lazy-components/src/app/dynamic/AsyncComponentLoader.ts
+++ lazy-components/src/app/dynamic/AsyncComponentLoader.ts
@@ -5,6 +5,10 @@ export class AsyncComponentLoader {
 
   load() {
     console.log('load: ' + this.loadFunc);
+    // legacy:
+    // load: ()=>__webpack_require__.e(366).then(__webpack_require__.bind(__webpack_require__,4366)).then(m=>m.PageBComponent)
+    // new builder:
+    // load: ()=>import("./chunk-CWC6WKNZ.mjs").then(o=>o.PageBComponent)
     return this.loadFunc();
   }
 }
